58b1dfaa39c8782652dcd9eaea5d819a7df8815a
hejian <hejian@fairlink.com>
2017-02-12 23:33:14 +0800

    put jplayer id in html temperary

--- src/main.jsx
+++ src/main.jsx
@@ -17,25 +17,22 @@ const routes = getRoutes();
 
 
 function withExampleBasename(history, dirname) {
-  var ret = useBasename(() => history)({ basename: dirname });
-  var location = ret.getCurrentLocation();
-  return ret;
+    var ret = useBasename(() => history)({ basename: dirname });
+    var location = ret.getCurrentLocation();
+    return ret;
 }
 
 
 ReactDOM.render((
-  <div>
     <Router history={withExampleBasename(browserHistory, browserHistory.getCurrentLocation().pathname)}>
-      <Route path="/" component={Home} />
-      <Route path="/react_page" component={ReactPage} />
-      <Route path="/form" component={Form} />
-      <Route path="/jmui" component={JMUI} />
-      <Route path="/animation" component={Animation} />
-      {routes}
-      <Route path="/music" component={Music} />
+        <Route path="/" component={Home} />
+        <Route path="/react_page" component={ReactPage} />
+        <Route path="/form" component={Form} />
+        <Route path="/jmui" component={JMUI} />
+        <Route path="/animation" component={Animation} />
+        {routes}
+        <Route path="/music" component={Music} />
     </Router>
-    <div id="jquery_jplayer_1" className="jp-jplayer"></div>
-  </div>
-), document.getElementById('root'), function(){
-  
+), document.getElementById('root'), function () {
+
 });
