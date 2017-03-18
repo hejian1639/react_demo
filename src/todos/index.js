7c83f76b44f7de592eb567f896b7c039e14b13ec
何健 <john.he@fairlinkcentury.com>
2017-03-18 12:14:13 +0800

    move todos index code

--- src/todos/index.js
+++ src/todos/index.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import App from './components/App'
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
+import { Button } from 'react-bootstrap';
+import 'css!bootstrap'
+import 'css!bootstrap-theme'
+import { connect } from 'react-redux'
 
+const setText = (text) => ({
+    type: 'SET_TEXT',
+    text
+})
 
-export default class Todos extends React.Component {
+class Todos extends React.Component {
 
     render() {
         return (
-                <App />
+            <div>
+                <AddTodo />
+                <VisibleTodoList />
+                <Footer />
+                {this.props.text}
+                <br />
+                <br />
+                <Button bsStyle="primary"
+                    onClick={e => {
+                        e.preventDefault();
+                        this.props.dispatch(setText('hello'));
+                    }}>
+                    refresh
+                </Button>
+            </div>
         );
     }
 }
 
+const mapStateToProps = (state) => ({
+  text: state.text
+})
+
+Todos = connect(mapStateToProps)(Todos)
+
+export default Todos
