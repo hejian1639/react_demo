a3050885e3eb8a13a9c044ab87ad802a2d3172e8
hejian <hejian@fairlink.com>
2017-03-18 09:57:54 +0800

    move reducer to root folder

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { useBasename } from 'History'
 import { Router, Route, IndexRoute, browserHistory } from 'ReactRouter'
 import $ from 'jquery'
 import 'jquery.jplayer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 
 
@@ -128,77 +131,81 @@ const rootRoute = {
     />
 ), document.getElementById('root'))*/
 
+const store = createStore(reducer)
+
 ReactDOM.render((
-    <Router history={withExampleBasename(browserHistory, '/')}>
-        <Route path="/" component={require('home').default} />
-        <Route path="/react_page" getComponent={(nextState, cb) => {
-            window.require(['dist/react-page'], (ReactPage) => {
-                cb(null, ReactPage.default);
-            });
-        }} />
-        <Route path="/form" getComponent={(nextState, cb) => {
-            window.require(['dist/form'], (Form) => {
-                cb(null, Form.default);
-            });
-        }} />
-        <Route path="/jmui" getComponent={(nextState, cb) => {
-            window.require(['dist/jmui-test'], (JMUI) => {
-                cb(null, JMUI.default);
-            });
-        }} />
-        <Route path="/animation" getComponent={(nextState, cb) => {
-            window.require(['dist/animation'], (Animation) => {
-                cb(null, Animation.default);
-            });
-        }} />
-
-        <Route path="/music" getComponent={(nextState, cb) => {
-            window.require(['dist/music'], (Music) => {
-                cb(null, Music.default);
-            });
-        }} />
-
-        <Route path="/starter_template" getComponent={(nextState, cb) => {
-            window.require(['dist/starter-template'], (StarterTemplate) => {
-                cb(null, StarterTemplate.default);
-            });
-        }} />
-
-        <Route path="/bootstrap_react" getComponent={(nextState, cb) => {
-            window.require(['dist/bootstrap-react'], (BootstrapReact) => {
-                cb(null, BootstrapReact.default);
-            });
-        }} />
-
-        <Route path="/bootstrap_popup*" getComponent={(nextState, cb) => {
-            window.require(['dist/bootstrap-popup'], (BootstrapPopup) => {
-                cb(null, BootstrapPopup.default);
-            });
-        }} />
-        <Route path="/todos*" getComponent={(nextState, cb) => {
-            window.require(['dist/todos'], (Todos) => {
-                cb(null, Todos.default);
-            });
-        }} />
-        <Route path="/kitchensink" getComponent={(nextState, cb) => {
-            window.require(['dist/kitchensink'], (Kitchensink) => {
-                cb(null, Kitchensink.Framework);
-            });
-        }} getIndexRoute={(nextState, cb) => {
-            window.require(['dist/kitchensink'], (Kitchensink) => {
-                cb(null, {
-                    getComponent(location, callback) {
-                        callback(null, Kitchensink.Index);
-                    }
+    <Provider store={store}>
+        <Router history={withExampleBasename(browserHistory, '/')}>
+            <Route path="/" component={require('home').default} />
+            <Route path="/react_page" getComponent={(nextState, cb) => {
+                window.require(['dist/react-page'], (ReactPage) => {
+                    cb(null, ReactPage.default);
                 });
-            });
-        }} getChildRoutes={(nextState, cb) => {
-            window.require(['dist/kitchensink'], (Kitchensink) => {
-                cb(null, Kitchensink.childRoutes);
-            });
-        }}/>
+            }} />
+            <Route path="/form" getComponent={(nextState, cb) => {
+                window.require(['dist/form'], (Form) => {
+                    cb(null, Form.default);
+                });
+            }} />
+            <Route path="/jmui" getComponent={(nextState, cb) => {
+                window.require(['dist/jmui-test'], (JMUI) => {
+                    cb(null, JMUI.default);
+                });
+            }} />
+            <Route path="/animation" getComponent={(nextState, cb) => {
+                window.require(['dist/animation'], (Animation) => {
+                    cb(null, Animation.default);
+                });
+            }} />
 
-    </Router>
+            <Route path="/music" getComponent={(nextState, cb) => {
+                window.require(['dist/music'], (Music) => {
+                    cb(null, Music.default);
+                });
+            }} />
+
+            <Route path="/starter_template" getComponent={(nextState, cb) => {
+                window.require(['dist/starter-template'], (StarterTemplate) => {
+                    cb(null, StarterTemplate.default);
+                });
+            }} />
+
+            <Route path="/bootstrap_react" getComponent={(nextState, cb) => {
+                window.require(['dist/bootstrap-react'], (BootstrapReact) => {
+                    cb(null, BootstrapReact.default);
+                });
+            }} />
+
+            <Route path="/bootstrap_popup*" getComponent={(nextState, cb) => {
+                window.require(['dist/bootstrap-popup'], (BootstrapPopup) => {
+                    cb(null, BootstrapPopup.default);
+                });
+            }} />
+            <Route path="/todos*" getComponent={(nextState, cb) => {
+                window.require(['dist/todos'], (Todos) => {
+                    cb(null, Todos.default);
+                });
+            }} />
+            <Route path="/kitchensink" getComponent={(nextState, cb) => {
+                window.require(['dist/kitchensink'], (Kitchensink) => {
+                    cb(null, Kitchensink.Framework);
+                });
+            }} getIndexRoute={(nextState, cb) => {
+                window.require(['dist/kitchensink'], (Kitchensink) => {
+                    cb(null, {
+                        getComponent(location, callback) {
+                            callback(null, Kitchensink.Index);
+                        }
+                    });
+                });
+            }} getChildRoutes={(nextState, cb) => {
+                window.require(['dist/kitchensink'], (Kitchensink) => {
+                    cb(null, Kitchensink.childRoutes);
+                });
+            }} />
+
+        </Router>
+    </Provider>
 ), document.getElementById('root'), function () {
 
 });
--- src/todos/index.js
+++ src/todos/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
 
-const store = createStore(reducer)
 
 export default class Todos extends React.Component {
 
     render() {
         return (
-            <Provider store={store}>
                 <App />
-            </Provider>
         );
     }
 }
