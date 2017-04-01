3000e0df71e23c13674f3d0ca9fae8cd033e6b90
何健 <john.he@fairlinkcentury.com>
2017-04-01 16:59:25 +0800

    make todos derive partial data from global

--- src/main.jsx
+++ src/main.jsx
@@ -10,131 +10,18 @@ import 'jquery.jplayer'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import Controller from './controller'
 
+import * as Actions from './actions'
 
 
 function withExampleBasename(history, dirname) {
     return useBasename(() => history)({ basename: dirname });
 }
 
-const rootRoute = {
-    childRoutes: [
-        {
-            path: '/',
-            component: require('home').default,
-        },
-        {
-            path: 'react_page',
-            getComponent(nextState, cb) {
-                window.require(['dist/react-page'], (ReactPage) => {
-                    cb(null, ReactPage.default);
-                });
-            }
-        },
-        {
-            path: '/form',
-            getComponent(nextState, cb) {
-                window.require(['dist/form'], (Form) => {
-                    cb(null, Form.default);
-                });
-            }
-        },
-        {
-            path: '/jmui',
-            getComponent(nextState, cb) {
-                window.require(['dist/jmui-test'], (JMUI) => {
-                    cb(null, JMUI.default);
-                });
-            }
-        },
-        {
-            path: '/animation',
-            getComponent(nextState, cb) {
-                window.require(['dist/animation'], (Animation) => {
-                    cb(null, Animation.default);
-                });
-            }
-        },
 
-        {
-            path: '/music',
-            getComponent(nextState, cb) {
-                window.require(['dist/music'], (Music) => {
-                    cb(null, Music.default);
-                });
-            }
-        },
-        {
-            path: '/starter_template',
-            getComponent(nextState, cb) {
-                window.require(['dist/starter-template'], (StarterTemplate) => {
-                    cb(null, StarterTemplate.default);
-                });
-            }
-        },
-        {
-            path: '/bootstrap_react',
-            getComponent(nextState, cb) {
-                window.require(['dist/bootstrap-react'], (BootstrapReact) => {
-                    cb(null, BootstrapReact.default);
-                });
-            }
-        },
-        {
-            path: '/bootstrap_popup*',
-            getComponent(nextState, cb) {
-                window.require(['dist/bootstrap-popup'], (BootstrapPopup) => {
-                    cb(null, BootstrapPopup.default);
-                });
-            }
-        },
-        {
-            path: '/todos*',
-            getComponent(nextState, cb) {
-                window.require(['dist/todos'], (Todos) => {
-                    cb(null, Todos.default);
-                });
-            }
-        },
-        {
-            path: '/kitchensink',
+const store = createStore(reducer);
 
-            getChildRoutes(location, cb) {
-                window.require(['dist/kitchensink'], (Kitchensink) => {
-                    cb(null, Kitchensink.childRoutes);
-                });
-            },
-            getIndexRoute(location, cb) {
-                window.require(['dist/kitchensink'], (Kitchensink) => {
-                    cb(null, {
-                        getComponent(location, callback) {
-                            callback(null, Kitchensink.Index);
-                        }
-                    });
-                    // cb(null, Kitchensink.Index);
-                });
-            },
 
-            getComponent(location, cb) {
-                window.require(['dist/kitchensink'], (Kitchensink) => {
-                    cb(null, Kitchensink.Framework);
-                });
-            }
-        },
-    ]
-}
-
-/*ReactDOM.render((
-    <Router
-        history={withExampleBasename(browserHistory, '/')}
-        routes={rootRoute}
-    />
-), document.getElementById('root'))*/
-
-const store = createStore(reducer)
-
-var controller = new Controller(store);
 
 ReactDOM.render((
     <Provider store={store}>
@@ -185,7 +72,13 @@ ReactDOM.render((
                 });
             }} />
             <Route path="/todos*" getComponent={(nextState, cb) => {
-                controller.openTodos(cb);
+
+                store.dispatch(Actions.setVisibilityFilter('1', 'SHOW_ALL'));
+                store.dispatch(Actions.setVisibilityFilter('2', 'SHOW_ALL'));
+                window.require(['dist/todos'], (Todos) => {
+                    cb(null, props => <Todos.default {...props} store={store} />);
+                });
+
             }} />
             <Route path="/kitchensink" getComponent={(nextState, cb) => {
                 window.require(['dist/kitchensink'], (Kitchensink) => {
--- src/todos/index.js
+++ src/todos/index.js
@@ -7,40 +7,110 @@ import 'css!bootstrap'
 import 'css!bootstrap-theme'
 import { connect } from 'react-redux'
 import * as Actions from '../actions'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import * as types from '../constants/ActionTypes'
 
+var App = ({ dispatch, text }) => {
 
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList id='1' />
+            <Footer id='1' />
+            <VisibleTodoList id='2' />
+            <Footer id='2' />
+            {text}
+            <br />
+            <br />
+            <Button bsStyle="primary"
+                onClick={e => {
+                    e.preventDefault();
+                    dispatch(Actions.setText('hello'));
+                }}>
+                refresh
+            </Button>
 
-class Todos extends React.Component {
-    
-
-    render() {
-        const { text, dispatch } = this.props;
-        return (
-            <div>
-                <AddTodo />
-                <VisibleTodoList id='1' />
-                <Footer id='1' />
-                <VisibleTodoList id='2' />
-                <Footer id='2' />
-                {text}
-                <br />
-                <br />
-                <Button bsStyle="primary"
-                    onClick={e => {
-                        e.preventDefault();
-                        dispatch(Actions.setText('hello'));
-                    }}>
-                    refresh
-                </Button>
-            </div>
-        );
-    }
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => ({
     text: state.text
 })
 
-Todos = connect(mapStateToProps)(Todos)
+App = connect(mapStateToProps)(App)
+
+
+class Todo {
+    constructor(text) {
+        this.id = Todo.nextTodoId++;
+        this.text = text;
+        this.completed = false;
+    }
+
+    toggle() {
+        this.completed = !this.completed;
+    }
+}
+
+Todo.nextTodoId = 0;
+
+
+const todosProxy = (store, state = [], action) => {
+    store.dispatch(action);
+    return store.getState().todos;
+
+}
+
+const visibilityFilter = (state = {}, action) => {
+    switch (action.type) {
+        case types.SET_VISIBILITY_FILTER:
+            state[action.id] = action.filter;
+            return { ...state };
+        default:
+            return state;
+    }
+}
+
+const text = (state = 'no data', action) => {
+    switch (action.type) {
+        case types.SET_TEXT:
+            return action.text
+        default:
+            return state
+    }
+}
+
+
+
+
+class Todos extends React.Component {
+
+
+    componentWillMount() {
+        const todos = todosProxy.bind(null, this.props.store);
+        const reducer = combineReducers({
+            todos,
+            visibilityFilter,
+            text
+        })
+        this.store = createStore(reducer);
+        this.store.dispatch(Actions.setVisibilityFilter('1', 'SHOW_ALL'));
+        this.store.dispatch(Actions.setVisibilityFilter('2', 'SHOW_ALL'));
+    }
+
+
+    render() {
+        return (
+            <Provider store={this.store}>
+                <App />
+
+            </Provider>
+
+        );
+    }
+}
+
 
 export default Todos
