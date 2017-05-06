8c9d6a60b24f7f267730aad1890878ef51e17f0f
何健 <john.he@fairlinkcentury.com>
2017-05-06 08:36:14 +0800

    enable iscroll on ios

--- src/2d-scroll/index.tsx
+++ src/2d-scroll/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from './style.css'
-// import IScroll from 'iscroll'
-import IScroll from "iscroll";
+import IScroll from 'iscroll'
 
+var u = navigator.userAgent;
+var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
 
 export default class IScroll2D extends React.Component<{}, {}> {
     componentDidMount () {
-       new IScroll('#wrapper', { scrollX: false, freeScroll: true });
+       new IScroll('#wrapper', { scrollX: true, freeScroll: true });
     }
 
 
@@ -17,6 +19,8 @@ export default class IScroll2D extends React.Component<{}, {}> {
 
                 <div className={style.wrapper} id="wrapper">
                     <div className={style.scroller}>
+                        <p>{'是否是Android：'+isAndroid}</p>
+                        <p>{'是否是iOS：'+isiOS}</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -31,7 +35,7 @@ export default class IScroll2D extends React.Component<{}, {}> {
                     </div>
                 </div>
 
-                <div className={style.footer}></div>
+                <div className={style.footer}>footer</div>
             </div>
         );
     }
