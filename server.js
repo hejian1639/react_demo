7f87694e62971f1a7b50f391daf21598990d0651
何健 <john.he@fairlinkcentury.com>
2017-05-31 21:52:25 +0800

    update server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import config from'./webpack.config.babel';
+import config from './webpack.config.babel';
 import path from 'path';
 import bodyParser from 'body-parser';
 import fs from 'fs';
@@ -16,25 +16,29 @@ app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath })
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  fs.readFile('index.html', function (err, data) {
-    if (err) {
-      res.status(404).send('Server.js > 404 - Page Not Found');
-      return;
-    }
-    console.log("异步读取: " + data);
-  	res.status(200).send(data.toString());
-  });
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+            res.status(404)
+                .send('Server.js > 404 - Page Not Found');
+            return;
+        }
+        // console.log("异步读取: " + data);
+        res.status(200)
+            .send(data.toString());
+    });
 
 });
 
 app.get('/favicon.ico', function(req, res) {
-  fs.readFile('favicon.ico', function (err, data) {
-    if (err) {
-      res.status(404).send('Server.js > 404 - Page Not Found');
-      return;
-    }
-  	res.status(200).send(data.toString());
-  });
+    fs.readFile('favicon.ico', function(err, data) {
+        if (err) {
+            res.status(404)
+                .send('Server.js > 404 - Page Not Found');
+            return;
+        }
+        res.status(200)
+            .send(data.toString());
+    });
 
 });
 
@@ -151,29 +155,42 @@ const NAV_LINKS2 = {
 };
 
 app.get('/service/home_list1', function(req, res) {
-		res.status(200).send(JSON.stringify(NAV_LINKS1));
+    res.status(200)
+        .send(JSON.stringify(NAV_LINKS1));
 });
 
 app.get('/service/home_list2', function(req, res) {
-		res.status(200).send(JSON.stringify(NAV_LINKS2));
+    res.status(200)
+        .send(JSON.stringify(NAV_LINKS2));
 });
 
 app.get('*', function(req, res) {
-	res.status(404).send('Server.js > 404 - Page Not Found');
+    // res.status(404).send('Server.js > 404 - Page Not Found');
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+            res.status(404)
+                .send('Server.js > 404 - Page Not Found');
+            return;
+        }
+        // console.log("异步读取: " + data);
+        res.status(200)
+            .send(data.toString());
+    });
 });
 
 app.use((err, req, res, next) => {
-	console.error("Error on request %s %s", req.method, req.url);
-	console.error(err.stack);
-	res.status(500).send("Server error");
+    console.error("Error on request %s %s", req.method, req.url);
+    console.error(err.stack);
+    res.status(500)
+        .send("Server error");
 });
 
 process.on('uncaughtException', evt => {
-	console.log('uncaughtException: ', evt);
+    console.log('uncaughtException: ', evt);
 });
 
-app.listen(3001, function(){
-	console.log('Listening on port 3001');
+app.listen(3001, function() {
+    console.log('Listening on port 3001');
 });
 
 // new WebpackDevServer(webpack(config), {
